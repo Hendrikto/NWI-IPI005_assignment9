dba246a7e2b48118fcd0a5c06b571c088c539c17
Hendrik Werner <hendrik.to@gmail.com>
2016-04-14 21:54:32 +0200

    Add an implementation of the FormVisitor interface * This implementation takes a tree of formulas and turns it into a String.

---
+++ PrintFormVisitor.java
@@ -0,0 +1,35 @@
+package assignment9;
+
+/**
+ *
+ * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
+ */
+public class PrintFormVisitor implements FormVisitor {
+
+    @Override
+    public void visit(BinaryOperatorForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visit(TrueForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visit(FalseForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visit(NotForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visit(AtomicForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
