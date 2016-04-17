e785a7e7291612fa4b1cf66b3fc49f0da9cc9ea1
Hendrik Werner <hendrik.to@gmail.com>
2016-04-17 20:26:51 +0200

    Add JavaDoc.

--- Form.java
+++ Form.java
@@ -1,12 +1,19 @@
 package assignment9;
 
 /**
+ * An interface for representing logical formulas. An extension of the Visitable
+ * interface.
  *
  * @author Hendrik Werner // s4549775
  * @author Jasper Haasdijk // s4449754
  */
 public interface Form extends Visitable {
 
+    /**
+     * Get the priority of this formula.
+     *
+     * @return the priority of this formula
+     */
     public int getPriority();
 
 }
