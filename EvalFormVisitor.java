5099ec4a1b3edc24b7978266ec17bf0d7102386f
Hendrik Werner <hendrik.to@gmail.com>
2016-04-17 16:56:47 +0200

    Add an implementation of FormVisitor that evaluates formulas.

---
+++ EvalFormVisitor.java
@@ -0,0 +1,48 @@
+package assignment9;
+
+import java.util.Map;
+
+/**
+ *
+ * @author Hendrik Werner // s4549775
+ * @author Jasper Haasdijk // s4449754
+ */
+public class EvalFormVisitor implements FormVisitor<Boolean> {
+
+    private final Map<String, Boolean> environment;
+
+    public EvalFormVisitor(Map<String, Boolean> env) {
+        this.environment = env;
+    }
+
+    @Override
+    public Boolean visit(BinaryOperatorForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Boolean visit(TrueForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Boolean visit(FalseForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Boolean visit(NotForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Boolean visit(AtomicForm form) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getString() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
