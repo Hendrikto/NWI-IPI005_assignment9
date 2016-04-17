e3ebe2befd248bcd8a756e77b773e93f3b2782b8
Hendrik Werner <hendrik.to@gmail.com>
2016-04-17 21:51:07 +0200

    Add JavaDoc.

--- Visitable.java
+++ Visitable.java
@@ -1,12 +1,20 @@
 package assignment9;
 
 /**
+ * An interface for accepting FormVisitors.
  *
  * @author Hendrik Werner // s4549775
  * @author Jasper Haasdijk // s4449754
  */
 public interface Visitable {
 
+    /**
+     * Accept a FormVisitor.
+     *
+     * @param <T> the return type
+     * @param visitor the FormVisitor to accept
+     * @return relayed return value of visitor.accept(this)
+     */
     public <T> T accept(FormVisitor<T> visitor);
 
 }
