2c62d82392afded012def213c21beedd1f967079
Hendrik Werner <hendrik.to@gmail.com>
2016-04-17 21:48:04 +0200

    Add JavaDoc.

--- TrueForm.java
+++ TrueForm.java
@@ -1,17 +1,28 @@
 package assignment9;
 
 /**
+ * A true formula.
  *
  * @author Hendrik Werner // s4549775
  * @author Jasper Haasdijk // s4449754
  */
 public class TrueForm implements Form {
 
+    /**
+     * Accept a Visitor.
+     *
+     * @param <T> the return type
+     * @param visitor the Visitor to accept
+     * @return relayed return value of visitor.visit(this)
+     */
     @Override
     public <T> T accept(FormVisitor<T> visitor) {
         return visitor.visit(this);
     }
 
+    /**
+     * @return the priority of this Form
+     */
     @Override
     public int getPriority() {
         return -1;
