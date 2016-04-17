0067fe7d6444b8dcf2a3acd10cb665db1d66d1b4
Hendrik Werner <hendrik.to@gmail.com>
2016-04-17 20:12:37 +0200

    Add JavaDoc.

--- BinaryOperatorForm.java
+++ BinaryOperatorForm.java
@@ -1,6 +1,7 @@
 package assignment9;
 
 /**
+ * A binary operation consisting of a BinaryOperator and two Form operands.
  *
  * @author Hendrik Werner // s4549775
  * @author Jasper Haasdijk // s4449754
@@ -11,6 +12,13 @@ public class BinaryOperatorForm implements Form {
     private final Form leftOperand;
     private final Form rightOperand;
 
+    /**
+     * Constructor method.
+     *
+     * @param op the BinaryOperator
+     * @param l the first operand
+     * @param r the second operand
+     */
     public BinaryOperatorForm(BinaryOperator op, Form l, Form r) {
         this.operation = op;
         this.leftOperand = l;
@@ -38,11 +46,21 @@ public class BinaryOperatorForm implements Form {
         return operation;
     }
 
+    /**
+     * Accept a Visitor.
+     *
+     * @param <T> the return type
+     * @param visitor the Visitor to accept
+     * @return relayed return value of visitor.visit(this)
+     */
     @Override
     public <T> T accept(FormVisitor<T> visitor) {
         return visitor.visit(this);
     }
 
+    /**
+     * @return the priority
+     */
     @Override
     public int getPriority() {
         return this.operation.getPriority();
